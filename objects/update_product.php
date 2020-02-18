14e078d11f6494572ecbb209fee852fe5d59ebb3
jamila umar <jamilaibrahim9311@gmail.com>
2020-02-18 09:35:48 +0100

    first commit

---
+++ objects/update_product.php
@@ -0,0 +1,69 @@
+<?php
+
+// retrieve one product will be here 
+
+// get ID of the product to be edited
+
+$id = isset($_GET['id']) ? $_GET['id'] : die('ERROR: missing ID.');
+// include database and object files 
+include_once 'config/database.php';
+include_once 'objects/product.php';
+include_once 'objects/category.php';
+
+// get database connection 
+
+$database = new Database();
+$db = $database->getConnection();
+
+// prepare objects
+
+$product = new Product($db);
+
+$category = new Category($db);
+// set ID property of product to be edited
+$product->id = $id;
+// read the details of product to be edited
+$product->readOne();
+?>
+
+<!-- 'update product' form will be here -->
+
+<?php
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// set page header 
+
+$page_title = "Update Product";
+
+include_once "layout_header.php";
+
+// contents will be here
+
+echo "<div class='right-button-margin'>";
+echo "<a href='index.php' class='btn btn-default pull-right'>Read Products</a>";
+
+echo "</div>";
+
+// set page footer
+include_once "layout_footer.php";
